651c17b608b82f9ea7a503061016da3fa3a9135c
Aiden <Aiden.Liu@tron.network>
2020-12-01 18:19:48 +0800

    Move 'IllegalException.java' to org.tron.tronj.client.Exception.

--- client/src/main/java/org/tron/tronj/client/Exception/IllegalException.java
+++ client/src/main/java/org/tron/tronj/client/Exception/IllegalException.java
@@ -2,7 +2,7 @@ package org.tron.tronj.client.Exception;
 
 public class IllegalException extends  Exception {
     public IllegalException(){
-        super("The query failed, please check if the parameters are correct.");
+        super("Query failed, please check if the parameters are correct.");
     }
 
     public IllegalException(String message){
